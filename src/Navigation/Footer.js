c0c7cd475e5002761f32f0a716f1c7c8921f0b41
itarek99 <tarekulislam7060@gmail.com>
2022-12-05 19:15:02 +0600

    footer update

--- src/Navigation/Footer.js
+++ src/Navigation/Footer.js
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
     <div className='bg-gray-100 text-base-content'>
-      <footer className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-10 container mx-auto px-2'>
+      <footer className='container mx-auto grid grid-cols-1 gap-6 p-10 px-2 md:grid-cols-2 lg:grid-cols-4'>
         <div className='md:col-span-2'>
           <div className=''>
-            <h3 className='text-2xl font-bold mb-2'>CAR BAZAAR</h3>
+            <h3 className='mb-2 text-2xl font-bold'>CAR BAZAAR</h3>
             <p className='pr-40'>
               carbazaar.com is the leading search car venture in Bangladesh, that helps users buy and sell cars.
             </p>
@@ -12,31 +12,31 @@ const Footer = () => {
         </div>
         <div className='flex flex-col'>
           <span className='footer-title'>Services</span>
-          <a href='/' className='link link-hover'>
+          <a href='/' className='link-hover link mb-1 text-sm'>
             Buy Car
           </a>
-          <a href='/' className='link link-hover'>
+          <a href='/' className='link-hover link mb-1 text-sm'>
             Sell Car
           </a>
-          <a href='/' className='link link-hover'>
+          <a href='/' className='link-hover link mb-1 text-sm'>
             Compare Car
           </a>
-          <a href='/' className='link link-hover'>
+          <a href='/' className='link-hover link mb-1 text-sm'>
             News and Reviews
           </a>
         </div>
         <div className='flex flex-col'>
           <span className='footer-title'>About</span>
-          <a href='/' className='link link-hover'>
+          <a href='/' className='link-hover link mb-1 text-sm'>
             About us
           </a>
-          <a href='/' className='link link-hover'>
+          <a href='/' className='link-hover link mb-1 text-sm'>
             Contact Us
           </a>
-          <a href='/' className='link link-hover'>
+          <a href='/' className='link-hover link mb-1 text-sm'>
             Privacy Policy
           </a>
-          <a href='/' className='link link-hover'>
+          <a href='/' className='link-hover link mb-1 text-sm'>
             Cookie policy
           </a>
         </div>
